c4a643fe735bd9b11c36faa8022b61eca2a3281e
Airmancooma <107194958+Airmancooma@users.noreply.github.com>
2023-06-16 12:25:40 +0200

    Create main.py

---
+++ usr-lib-calamares/modules/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import libcalamares
+from libcalamares.utils import target_env_call
+
+def remove_airos(config):
+    user = config['user']
+    target_env_call(['deluser', '--remove-all-files', user])
+
+def run():
+    return remove_airos(libcalamares.job.configuration)
